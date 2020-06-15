01fb6d585003fdcca9ade5a8642159962ac8807f
AAVRITI <AAVRITI@AAVRITI-PC>
2020-06-15 20:01:02 +0530

    first commit

---
+++ src/git.java
@@ -0,0 +1,14 @@
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			String str1,str2;
+		
+		    str1="c";
+		    str2="b";
+			System.out.println(str2.compareTo(str1));
+	}
+
+}
